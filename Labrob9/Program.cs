c87a18d9bfa58cebb91e270fcbcbf5339e8e3819
Silver1999 <sergey27008@gmail.com>
2020-03-28 17:52:12 +0200

    first comit

---
+++ Labrob9/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labrob9
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
